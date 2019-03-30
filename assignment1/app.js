4d9a04176f78833cbe5ff64e722a036ec5c7f63e
barbarcohen <barbarcohen@seznam.cz>
2019-03-30 21:22:04 +0100

    assignment1, working

---
+++ assignment1/app.js
@@ -0,0 +1,25 @@
+(function () {
+"use strict";
+angular.module("LunchCheck", [])
+  .controller("LunchCheckController", LunchCheckController);
+
+LunchCheckController.$inject =  ["$scope", "$filter"];
+
+function LunchCheckController($scope, $filter){
+  $scope.menu = "";
+  $scope.checkDishes = function(){
+    if($scope.menu == ""){
+      $scope.checkMessage  =  "Please enter data first";
+    } else {
+      var dishesCount = $scope.menu.split(",").length;
+      if(dishesCount <= 3) {
+        $scope.checkMessage  = "Enjoy!";
+        $scope.checkMessageStyle = "green";
+      } else {
+        $scope.checkMessage  = "Too much!";
+        $scope.checkMessageStyle = "red";
+      }
+    }
+  };
+}
+})();
