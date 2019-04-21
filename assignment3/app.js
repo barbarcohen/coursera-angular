ed41920203d4cb5374d2e99355a2aef6671f892a
barbarcohen <barbarcohen@seznam.cz>
2019-04-21 16:20:47 +0200

    working ass3

--- assignment3/app.js
+++ assignment3/app.js
@@ -4,31 +4,40 @@
   angular.module("NarrowItDownApp", [])
   .controller("NarrowItDownController", NarrowItDownController)
   .service("MenuSerchService", MenuSerchService)
-  .constant("ApiBasePath","https://davids-restaurant.herokuapp.com");
+  .constant("ApiBasePath","https://davids-restaurant.herokuapp.com")
+  .directive("foundItems", FoundItems);
 
 
   NarrowItDownController.$inject = ["MenuSerchService", "$scope"];
   function NarrowItDownController(MenuSerchService, $scope){
     var narrowItController = this;
 
-    var found;
+    var found = [];
 
     narrowItController.getMatchedMenuItems = function(){
-      var promise = MenuSerchService.getMatchedMenuItems($scope.searchTerm);
-      promise.then(function(response){
-        found = response;
-      }).catch(function(error){
-        console.log("error while retrieving the data");
-        console.log(error);
-      });
+      var searchTerm = $scope.searchTerm
+      if(searchTerm == undefined || searchTerm.length == 0 ) {
+        found = new Array();
+      } else {
+        var promise = MenuSerchService.getMatchedMenuItems(searchTerm);
+        promise.then(function(response){
+          found = response;
+        }, function(error){
+          console.log("error while retrieving the data");
+        });
+      }
     };
 
-    narrowItController.list = function(){
-      return found;
-    }
-  };
+    narrowItController.remove = function (index){
+      found.splice(index, 1);
+    };
 
-  MenuSerchService.$inject = ["$http", "ApiBasePath"]
+    narrowItController.found = function(){
+      return found;
+    };
+  }
+
+  MenuSerchService.$inject = ["$http", "ApiBasePath"];
   function MenuSerchService($http, ApiBasePath){
     var service = this;
 
@@ -38,19 +47,36 @@
           url: (ApiBasePath + "/menu_items.json")
         }).then(function (response){
           // process result and only keep items that match
+          var foundItems = [];
+
           var items = response.data.menu_items;
-          var foundItems = new Array();
           for(var index = 0; index < items.length; ++index){
             if(items[index].description.includes(searchTerm)){
               foundItems.push(items[index]);
             }
           }
-          // return processed items
           return foundItems;
         });
       return response;
     };
+  }
 
+  function FoundItems(){
+    var ddo = {
+        restrict: "E",
+        templateUrl: "found-items.html",
+        scope: {
+          "foundItems": "<",
+          "onRemove": "&",
+        },
+        controller: FoundItemsController,
+        controllerAs: "dirController",
+        bindToController: true
+    };
+    return ddo;
+  }
 
+  function FoundItemsController(){
+    var dirController = this;
   };
 })();
